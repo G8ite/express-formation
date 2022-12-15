6218103f6c28ee588bfbcdb382e31ffc2ab26a83
Amandine Pimentel de Andrade <70423300+G8ite@users.noreply.github.com>
2022-12-15 10:58:46 +0100

    Feat : ajout de l'environnement et des routers api et post

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from "./config.js";
+import { routerApi } from "./routers/api.rooter.js";
+
+mongoose.set('strictQuery', true);
+mongoose.connect(`mongodb+srv://${config.db.user}:${config.db.password}@${config.db.host}/?retryWrites=true&w=majority`)
+        .then(() => console.log('connexion à mongo réussie'))
+        .catch(err => console.log(err))
 
 const app = express();
+
+app.use('/api', routerApi)
+
+app.get('/', (req, res)=>{
+    res.send('hello word')
+}) 
 
 app.listen(3000, console.log('listen 3000'))
